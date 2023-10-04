d88529968fc108da8dda980b1d248ec55c08425f
An <134388379+anhhdev2203@users.noreply.github.com>
2023-10-04 16:50:09 +0700

    loi header

--- src/pages/Header/index.jsx
+++ src/pages/Header/index.jsx
@@ -1,8 +1,38 @@
+import { AppBar, Box, List, ListItem, Toolbar, Typography } from '@mui/material'
 import React from 'react'
+import elaine from "../../assets/image/elaine.png"
+import people from "../../assets/image/logochandung.png"
+import triangle from "../../assets/image/logotamgiac.png"
 
 function Header() {
   return (
-    <div>Header</div>
+    <>
+    {/* <Box>
+    <img
+        src={elaine}
+      />
+      <List>
+        <ListItem>An Hoang</ListItem>
+        <img src = {people}></img>
+        <img src ={triangle}></img>
+      </List>
+    </Box> */}
+
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar color="transparent" component="nav"> 
+        <Toolbar>
+        <img
+        src={elaine}
+        />
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            An Hoang
+          </Typography>
+          <img src = {people}></img>
+        <img src ={triangle}></img>
+        </Toolbar>
+      </AppBar>
+    </Box>
+    </>
   )
 }
 
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,13 +1,14 @@
+import { Box } from '@mui/material'
 import React from 'react'
 import { Link } from 'react-router-dom'
 
 function Home() {
   return (
-    <>
+    <Box sx ={{background: "red"}}>
         <h1>Day la home page</h1>
         <Link to = "/login"><button>Login</button></Link>
 
-    </>
+    </Box>
   )
 }
 
