d02f3c6708a08bf9eca2e9dcc2bc33ed9c694bf1
An <134388379+anhhdev2203@users.noreply.github.com>
2023-10-04 23:22:34 +0700

    loi man hinh

--- src/pages/Header/index.jsx
+++ src/pages/Header/index.jsx
@@ -1,39 +1,49 @@
-import { AppBar, Box, List, ListItem, Toolbar, Typography } from '@mui/material'
-import React from 'react'
-import elaine from "../../assets/image/elaine.png"
-import people from "../../assets/image/logochandung.png"
-import triangle from "../../assets/image/logotamgiac.png"
-
+import { AppBar, Box, Container, Icon, Toolbar, Typography } from "@mui/material";
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import { styled } from "@mui/material/styles";
+import React from "react";
+import elaine from "../../assets/image/elaine.png";
+import people from "../../assets/image/logochandung.png";
+import triangle from "../../assets/image/logotamgiac.png";
+const Con = styled(Container)({
+  maxWidth: "1440px",
+  width: "100%",
+});
 function Header() {
   return (
     <>
-    {/* <Box>
-    <img
-        src={elaine}
-      />
-      <List>
-        <ListItem>An Hoang</ListItem>
-        <img src = {people}></img>
-        <img src ={triangle}></img>
-      </List>
-    </Box> */}
-
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar color="transparent" component="nav"> 
-        <Toolbar>
-        <img
-        src={elaine}
-        />
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            An Hoang
-          </Typography>
-          <img src = {people}></img>
-        <img src ={triangle}></img>
-        </Toolbar>
-      </AppBar>
-    </Box>
+      <Box sx={{ width: "100vw", position : "fixed"}}>
+        <Container maxWidth="lg">
+          <AppBar color="transparent" position="static" sx ={{boxShadow: "none"}}>
+            <Toolbar>
+              <img
+                src={elaine}
+                alt="Elaine"
+                style={{
+                  width: "198px",
+                  height: "111px",
+                }}
+              />
+              <Typography
+                variant="h6"
+                component="div"
+                sx={{ flex: 1, display: "flex", justifyContent: "end" }}
+              >
+                An Hoang
+              </Typography>
+              {/* <img
+                style={{ width: " 124px", height: "64px" }}
+                src={people}
+              ></img> */}
+              <PersonOutlineOutlinedIcon sx={{width: "124px",height: "64px"}}></PersonOutlineOutlinedIcon>
+              <ArrowDropDownIcon sx = {{width: "124px",height: "64px"}}></ArrowDropDownIcon>
+            </Toolbar>
+          </AppBar>
+        </Container>
+      </Box>
     </>
-  )
+  );
 }
 
-export default Header
+export default Header;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,15 +1,143 @@
-import { Box } from '@mui/material'
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { DisplaySettings } from "@mui/icons-material";
+import { Box, Button, Card, Container, Stack, Typography } from "@mui/material";
+import React from "react";
+import CardMedia from "@mui/material/CardMedia";
+import { Link } from "react-router-dom";
+import luutru from "../../assets/image/luutru1.jpg";
+import nhatkiluutru from "../../assets/image/nhatkiyeuthich.jpg";
+import taonhatki from "../../assets/image/taonhatki.jpg";
+import thongke from "../../assets/image/thongke.jpg";
+import traodoinhatki from "../../assets/image/traodoinhatki.jpg";
+import { styled } from "@mui/material/styles";
+const CardItem = styled (Card) ({
+  width: "380px",
+  //height: "216px",
+  backgroundColor: "#E6F6CB",
+  borderRadius: "25.967px",
+  display: "flex",
+  justifyContent: "center",
+  flexDirection: "column",
+  alignItems: "center",
 
+});
+const buttonText = styled(Typography)({
+  color:" #79AC78",
+  fontFamily: "Roboto",
+  fontSize: "32px",
+  fontStyle: "normal",
+  fontWeight: 700,
+  lineHeight: "normal",
+  padding: "8px"
+});
 function Home() {
   return (
-    <Box sx ={{background: "red"}}>
-        <h1>Day la home page</h1>
-        <Link to = "/login"><button>Login</button></Link>
+    <Box
+      sx={{
+        width: "100vw",
+        height: "100vw",
+        background: "#F5CFCF",
+        paddingTop: "111px",
+      }}
+    >
+      <Container maxWidth="xl">
+        <Typography
+          sx={{
+            color: "#D069B3",
+            fontFamily: "Dancing Script",
+            fontSize: "90px",
+            fontStyle: "normal",
+            fontWeight: 700,
+            lineHeight: "normal",
+            paddingBottom: "56px",
+          }}
+        >
+          {" "}
+          Trang chủ
+        </Typography>
+        <Stack>
+          <Stack display="flex " flexDirection="row" gap="55px" justifyContent= "center">
+            
+            <CardItem
+             
+            >
+              <CardMedia component="img" image={taonhatki}  />
+              <Button sx={{
+                  backgroundColor: "#EFB49599",
+                  display: "flex",
+                  alignSelf: "center",
+                }}><buttonText>Tạo nhật kí</buttonText></Button>
+            </CardItem>
+            <CardItem
+            >
+              <CardMedia
+                sx={{ MozBorderRadiusTopleft: "25.967px" }}
+                component="img"
+                image={luutru}
+              />
+              <Button sx={{
+                  backgroundColor: "#EFB49599",
+                  display: "flex",
+                  alignSelf: "center",
+                }}><buttonText>Lưu trữ</buttonText></Button>
+            </CardItem>
+            <CardItem
+            >
+              <CardMedia component="img" image={nhatkiluutru} />
+              <Button
+                sx={{
+                  backgroundColor: "#EFB49599",
+                  display: "flex",
+                  alignSelf: "center",
+                }}
+              >
+                <buttonText>Nhật kí đáng nhớ</buttonText>
+              </Button>
+            </CardItem>
+          </Stack>
 
+          <Typography
+            sx={{
+              color: "#000",
+              fontFamily: "Roboto",
+              fontSize: "36px",
+              fontStyle: "normal",
+              fontweight: 300,
+              lineHeight: "normal",
+              padding: "50px",
+              width: "600px",
+              display :"flex",
+              alignSelf: "center",
+              textAlign: "center"
+            }}
+          >
+            “Cuộc sống xô bồ cũng đừng quên dành thời gian cho bản thân nhé!”
+          </Typography>
+
+          <Stack display="flex " flexDirection="row" gap="55px" justifyContent="center">
+            <CardItem
+            >
+              <CardMedia component="img" image={traodoinhatki} />
+              <Button sx={{
+                  backgroundColor: "#EFB49599",
+                  display: "flex",
+                  alignSelf: "center",
+                }}><buttonText>Trao đổi nhật kí</buttonText></Button>
+            </CardItem>
+
+            <CardItem
+            >
+              <CardMedia component="img" image={thongke} />
+              <Button sx={{
+                  backgroundColor: "#EFB49599",
+                  display: "flex",
+                  alignSelf: "center",
+                }}><buttonText>Thống kê</buttonText></Button>
+            </CardItem>
+          </Stack>
+        </Stack>
+      </Container>
     </Box>
-  )
+  );
 }
 
-export default Home
+export default Home;
