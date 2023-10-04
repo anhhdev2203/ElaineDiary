94c4b65cac7db83b1eb894a88c80a847af5e8be3
Saugao274 <thuynttde170035@fpt.edu.vn>
2023-10-04 22:57:56 +0700

    sửa header cao hơn xí

--- src/pages/Header/index.jsx
+++ src/pages/Header/index.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 function Header() {
-  return (
-    <div>Header</div>
-  )
+  return <div style={{ margin: "10px" }}>Header</div>;
 }
 
-export default Header
+export default Header;
