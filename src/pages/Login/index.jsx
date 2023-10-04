89272a94146f2cfed166851161398eae2a16420b
Trinh Nguyen <137372274+zTrinh121@users.noreply.github.com>
2023-10-04 23:45:51 +0700

    Register ang login

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,11 +1,148 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { Box, Button, Slide, Stack, TextField, Typography } from '@mui/material';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import Logo from '../../assets/image/logoSignup.png'
+import Contact from '../../assets/image/decorContactSignup.png'
+import '../../App.css'
+import './login.css'
+
+import createSlider from './slide';
+
 
 function Login() {
   return (
     <>
-        <h1>Day la login page</h1>
-        <Link to ="/"><button>Back to Home</button></Link>
+      <style>
+        {`
+          html, body {
+            margin: 0;
+            padding: 0;
+            overflow: hidden;
+            height: 100%;
+          }
+        `}
+      </style>
+      
+      <Stack direction="row"
+        sx={{
+          backgroundColor: "#FFF6DC",
+          height: "100vh",
+          position: 'relative'
+        }}
+      >
+        <Box 
+        sx={{
+          width:"50%"
+        }}
+        >
+          <Box
+            component="img"
+            alt="Logo"
+            src={Logo}
+            sx={{
+              position: 'absolute',
+              top: 0,
+              left: 0,
+              width: "24vh",
+
+            }}
+          />
+          <Box
+            component="img"
+            alt="Contact"
+            src={Contact}
+            sx={{
+              position: 'absolute',
+              top: 0,
+              right: "50%",
+              width: "12vh",
+            }}
+          />
+
+          <Box className="word"
+            sx={{
+              width: "100%",
+              marginBottom: "8vh",
+              marginTop: "24vh",
+                          
+            }}>
+            <Typography
+              sx={{
+                fontFamily: "Clicker Script",
+                fontStyle: "normal",
+                textAlign: "center",
+                lineHeight: "normal",
+                fontWeight: 400,
+                fontSize: 64,
+                
+
+              }}
+            >I'm here...</Typography>
+            <Typography
+              sx={{
+                textAlign: 'center',
+                fontFamily: 'Roboto',
+                fontSize: 12
+              }}
+            >Đây là miền kí ức của bạn</Typography>
+          </Box>
+
+          <Stack
+            direction="column"
+            spacing={2}
+            sx={{              
+              width:"70%",
+              marginLeft:"5%",
+              alignItems: 'center',
+            }}
+          >
+            <TextField id="outlined-basic" label="Tên người dùng" variant="outlined"
+
+            />
+            <TextField id="outlined-password-input"
+              label="Mật khẩu"
+              type="password"
+              autoComplete="off"
+              // autoComplete="current-password"
+            />
+            <Button variant="contained">Đăng nhập</Button>
+            <Stack direction="row" spacing={4} alignSelf="center">
+              <Typography
+                sx={{
+                  color: "#695252",
+                  fontFamily: "Be Vietnam Pro",
+                  fontSize: "12px"
+                }}
+              >Bạn chưa có tài khoản?</Typography>
+              <Link to='/register'
+                sx={{
+                  color: "#5D579A",
+                  fontFamily: "Be Vietnam Pro",
+                  fontSize: "12px",
+                  fontWeight: "bold"
+                }}
+              >Đăng kí</Link>
+            </Stack>
+          </Stack>
+        </Box>
+        <Box
+          width="50%"
+          height="100vh"
+          position="absolute"
+          overflow="hidden"
+          top="0"
+          right="0"
+        >
+          {createSlider()}
+
+        </Box>
+
+      </Stack>
+
+      <Box
+
+      ></Box>
     </>
   )
 }
--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -1,8 +1,182 @@
-import React from 'react'
-
+import { useState } from 'react'
+import { Stack, Button, Box, Chip, Typography, TextField } from '@mui/material';
+import Decor from '../../assets/image/decorRegister.png'
+import DecorBook from '../../assets/image/decorBookRegister.png'
+import Label from '../../assets/image/labelRegister.png'
+import './register.css'
+import { Link } from 'react-router-dom';
 function Register() {
   return (
-    <div>Register</div>
+    <>
+      <style>
+        {`
+          html, body {
+            margin: 0;
+            padding: 0;
+            overflow: hidden;
+            height: 100%;
+          }
+        `}
+      </style>
+      <Stack direction="row"
+        sx={{
+          height: "100vh"
+        }}
+      >
+        <Box
+          sx={{
+            width: "35%",
+            height: "100%",
+            position:"relative",
+            backgroundColor:"#DBD8BA",
+          }}
+        >
+
+          <Box
+            component="img"
+            alt="Decor"
+            src={Decor}
+            sx={{
+              height: "75%",
+              position:'absolute',
+              
+            }}
+          />
+
+          <Box>
+            <Chip label="Keep it secret"
+              sx={{
+                backgroundColor: "#899871",
+                borderRadius: "50px",
+                position:'absolute',
+                typography: {
+                  fontFamily: "Roboto",
+                  color: "#fff"
+                }
+              }}
+            />
+          </Box>
+
+        </Box>
+
+
+        <Box
+          sx={{
+            width: "65%",
+            height: "100vh",
+            backgroundColor: "#659282",
+          }}
+        >
+          <Stack direction="row" spacing={2}
+            sx={{
+              justifyContent: "space-between",
+              alignItems: "flex-end",
+              marginTop: "1vh",
+              marginLeft: "2vh"
+            }}
+          >
+
+            <Box
+              component="img"
+              src={DecorBook}
+              sx={{
+                width: "16vh"
+              }}
+            />
+            <Box
+              component="img"
+              src={Label}
+              sx={{
+                width: "16vh"
+
+              }}
+            />
+          </Stack>
+          <Stack direction="column" spacing={1} alignItems="center" marginTop="-12vh">
+            <Typography
+              sx={{
+                color: "#fff",
+                fontFamily: "Dancing Script",
+                fontSize: "64px"
+              }}
+            >Đăng kí
+            </Typography>
+            <Typography
+              sx={{
+                color: "#fff",
+                fontFamily: "Roboto",
+                fontSize: "12px",
+                fontStyle: 'normal',
+              }}
+            >
+              Cho tôi biết thông tin của bạn nào...
+            </Typography>
+          </Stack>
+
+          <Stack direction="column" spacing={2} alignItems="center"
+          sx={{
+            marginTop:"4vh", 
+         
+          }}
+          >
+           
+            <TextField id="outlined-basic" label="Tên người dùng" variant="outlined" size="small"
+            sx={{
+               width:400,
+              maxWidth:"80%",
+            }}
+            />
+            <TextField id="outlined-basic" label="Năm sinh" variant="outlined" size="small"
+             sx={{
+               width:400,
+               maxWidth:"80%"
+            }}
+            />
+            <TextField id="outlined-password-input"
+              label="Mật khẩu"
+              type="password" size="small"
+              // autoComplete="current-password"
+              sx={{
+                width:400,
+                maxWidth:"80%"
+             }}
+            />
+             <TextField id="outlined-password-input"
+              label="Nhập lại mật khẩu"
+              type="password" size="small"
+              autoComplete="off"
+              // autoComplete="current-password"
+              sx={{
+                width:400,
+                maxWidth:"80%"
+             }}
+            />
+            <Button variant="contained"
+            sx={{
+              display:'block',
+              textTransform:"none",
+              
+            }}
+            >Đăng kí</Button>
+            <Stack direction="row" spacing={2}>
+              <Typography
+              sx={{
+                color: "#695252",
+                fontFamily: "Be Vietnam Pro",
+                fontSize:"12px"
+              }}
+              >Bạn đã có tài khoản?</Typography>
+              <Link to='/login'             
+              >Đăng nhập</Link>
+            </Stack>
+            
+
+          </Stack>
+
+        </Box>
+
+      </Stack>
+    </>
   )
 }
 
