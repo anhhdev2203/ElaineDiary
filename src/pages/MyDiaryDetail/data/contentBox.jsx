3b03b5ea3376db628e28331da6f921e09983aca5
Saugao274 <thuynttde170035@fpt.edu.vn>
2023-10-05 18:14:42 +0700

    add img, responstive content

--- src/pages/MyDiaryDetail/data/contentBox.jsx
+++ src/pages/MyDiaryDetail/data/contentBox.jsx
@@ -3,21 +3,46 @@ import {
   ListItemText,
   Divider,
   Typography,
-  Grid,
-  ButtonBase,
   styled,
   Container,
 } from "@mui/material";
 import "../img/cover.png";
 import coverImage from "../img/cover.png";
+import feelImage from "../img/HAPPY.png";
+import Grid from "@mui/material/Unstable_Grid2";
 
 const Img = styled("img")({
-  height: "100%",
+  // height: "100%",
   // width: "50%",
-  position: "absolute",
-  top: 0,
-  left: 0,
+  // position: "absolute",
+  // top: 0,
+  // left: 0,
 });
+const responsiveContent = {
+  "@media (max-width: 320px)": {
+    //điện thoại
+    fontSize: "0rem",
+    backgroundColor: "#C2BCC5",
+  },
+  "@media (min-width: 320px,max-width: 768px)": {
+    //may tinh bảng nhỏ
+    fontSize: "1rem",
+  },
+  "@media (min-width: 768px,max-width: 992px)": {
+    //may tinh bang lon
+    fontSize: "1rem",
+  },
+  "@media (min-width: 992px,max-width: 1200px)": {
+    //may tinh thong thuong
+    fontSize: "1rem",
+  },
+  "@media (min-width: 1200px)": {
+    //lap top to
+    backgroundColor: "#red",
+
+    fontSize: "2.5rem",
+  },
+};
 export const ContentBox = ({ title, content }) => {
   return (
     <Box
@@ -29,6 +54,7 @@ export const ContentBox = ({ title, content }) => {
         boxShadow: "12px 12px 0px 0px  #30173F",
         backgroundColor: "#C2BCC5",
         borderRadius: "24px",
+        // border: "none",
 
         // transform: "scale(1)",
         // transition: "transform 0.3s ease-in-out",
@@ -37,25 +63,58 @@ export const ContentBox = ({ title, content }) => {
         // },
       }}
     >
-      <Box variant="h5" sx={{ direction: "row" }}>
+      <Box
+        sx={{
+          direction: "row",
+        }}
+      >
         <Container>
-          <ListItemText variant="h5" sx={{ mb: 1 }} primary={title} />
+          <ListItemText
+            variant="h5"
+            sx={{
+              mb: 1,
+            }}
+            primary={title}
+          />
         </Container>
         <Divider />
 
-        {/* <Container sx={{ position: "relative" }}> */}
-        <ButtonBase>
-          <Img alt="cover" src={coverImage} />
-          <Typography
-            sx={{
-              // marginLeft: "3%",
-              textAlign: "left",
-            }}
+        <Grid
+          container
+          spacing={1}
+          minHeight={160}
+          sx={{ position: "relative" }}
+        >
+          <Grid
+            xs
+            display="flex"
+            justifyContent="space-between"
+            alignItems="center"
           >
-            {content}
-          </Typography>
-        </ButtonBase>
-        {/* </Container> */}
+            <Img alt="cover" src={coverImage} />
+          </Grid>
+
+          <Grid
+            xs
+            display="flex"
+            justifyContent="center"
+            alignItems="flex-start"
+          >
+            <Typography
+              sx={{
+                ...responsiveContent,
+                // marginLeft: "3%",
+                textAlign: "left",
+              }}
+            >
+              {content}
+            </Typography>
+          </Grid>
+
+          <Grid xs display="flex" justifyContent="flex-end" alignItems="end">
+            <Img alt="fell" src={feelImage} />
+          </Grid>
+        </Grid>
       </Box>
     </Box>
   );
